c222ad57cdc32ab08e5d6086adf90bb88af9172e
fanlumaster <1730976608@qq.com>
2024-07-17 04:54:39 +0800

    adapt to the new configuration of which-key

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -3,31 +3,18 @@ return {
   event = "VeryLazy",
   opts = {
     plugins = { spelling = true },
-    defaults = {
-      mode = { "n", "v" },
-      ["g"] = { name = "+goto" },
-      ["gs"] = { name = "+surround" },
-      ["]"] = { name = "+next" },
-      ["["] = { name = "+prev" },
-      ["<leader><tab>"] = { name = "+tabs" },
-      ["<leader>b"] = { name = "+buffer" },
-      ["<leader>f"] = { name = "+file/find" },
-      ["<leader>g"] = { name = "+git" },
-      ["<leader>gh"] = { name = "+hunks" },
-      ["<leader>l"] = { name = "+lsp" },
-      ["<leader>p"] = { name = "+python" },
-      ["<leader>q"] = { name = "+quit/session" },
-      ["<leader>s"] = { name = "+search" },
-      ["<leader>t"] = { name = "+toggle-term" },
-      ["<leader>u"] = { name = "+ui" },
-      ["<leader>w"] = { name = "+windows" },
-      ["<leader>x"] = { name = "+diagnostics/quickfix" },
-      ["<leader><leader>"] = { name = "+hop/toggle-color" },
-    },
   },
   config = function(_, opts)
     local wk = require("which-key")
     wk.setup(opts)
-    wk.register(opts.defaults)
+    wk.add({
+      {
+        mode = { "n", "v" },
+        { "<leader>l", group = "+lsp" },
+        { "<leader>p", group = "python" },
+        { "<leader>t", group = "+toggle-term" },
+        { "<leader><leader>t", group = "+hop/toggle-color" },
+      },
+    })
   end,
 }
