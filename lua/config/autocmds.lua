b0321907de8307904fd3f996ebae1c5585a4b767
fanlumaster <1730976608@qq.com>
2024-10-02 06:32:34 +0800

    change cpp shift tab width

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -26,9 +26,18 @@ vim.api.nvim_create_autocmd("FileType", {
 -- change c/cpp file tab width to 4 spaces
 vim.api.nvim_create_autocmd("FileType", {
   group = augroup("associate_filetype"),
-  pattern = { "c", "cpp", "rust", "python" },
+  pattern = { "c", "rust", "python" },
   callback = function()
     vim.opt.shiftwidth = 4
+    vim.opt.formatoptions:remove({ "o" }) -- 防止使用 o 切换到下一行的时候自动加上注释符号(在上一行是注释的情况下)
+  end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  group = augroup("associate_filetype"),
+  pattern = { "cpp"},
+  callback = function()
+    vim.opt.shiftwidth = 2
     vim.opt.formatoptions:remove({ "o" }) -- 防止使用 o 切换到下一行的时候自动加上注释符号(在上一行是注释的情况下)
   end,
 })
