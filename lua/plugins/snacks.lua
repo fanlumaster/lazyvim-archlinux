4f0a2e32f3e2c238523f5e9399c39654a6e1e622
fanlumaster <1730976608@qq.com>
2024-12-24 14:46:11 +0800

    set rainbow-like colors for indent scope(vertical lines)

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -63,5 +63,45 @@ return {
       },
     },
     scroll = { enabled = false },
+    indent = {
+      indent = {
+        priority = 1,
+        enabled = true, -- enable indent guides
+        char = "│",
+        only_scope = false, -- only show indent guides of the scope
+        only_current = true, -- only show indent guides in the current window
+        -- hl = "SnacksIndent8", ---@type string|string[] hl groups for indent guides
+        -- can be a list of hl groups to cycle through
+        -- hl = {
+        --   "SnacksIndent1",
+        --   "SnacksIndent2",
+        --   "SnacksIndent3",
+        --   "SnacksIndent4",
+        --   "SnacksIndent5",
+        --   "SnacksIndent6",
+        --   "SnacksIndent7",
+        --   "SnacksIndent8",
+        -- },
+      },
+      ---@class snacks.indent.Scope.Config: snacks.scope.Config
+      scope = {
+        enabled = true, -- enable highlighting the current scope
+        priority = 200,
+        char = "│",
+        underline = false, -- underline the start of the scope
+        only_current = true, -- only show scope in the current window
+        -- hl = "SnacksIndentScope", ---@type string|string[] hl group for scopes
+        hl = {
+          "SnacksIndent1",
+          "SnacksIndent2",
+          "SnacksIndent3",
+          "SnacksIndent4",
+          "SnacksIndent5",
+          "SnacksIndent6",
+          "SnacksIndent7",
+          "SnacksIndent8",
+        },
+      },
+    },
   },
 }
