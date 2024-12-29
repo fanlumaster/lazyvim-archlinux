18444699ed90c6656a8ac9f0e8012d2d813ec70e
fanlumaster <1730976608@qq.com>
2024-12-29 19:56:44 +0800

    adapt to vscode

--- init.lua
+++ init.lua
@@ -2,10 +2,12 @@
 require("config.lazy")
 
 require("fany.fanykeymaps")
-require("fany.fanytelescope")
 require("fany.fanyconfig")
-require("fany.fanyformat")
-require("fany.utils.fanyutils").set_os_info()
-require("fany.fanysnippets")
-require("fany.fanyneotree")
-require("fany.fanytoggleterm")
+if not vim.g.vscode then
+  require("fany.fanytelescope")
+  require("fany.fanyformat")
+  require("fany.utils.fanyutils").set_os_info()
+  require("fany.fanysnippets")
+  require("fany.fanyneotree")
+  require("fany.fanytoggleterm")
+end
